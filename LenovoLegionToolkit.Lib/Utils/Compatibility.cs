bcfab0d01553629db752cd3e620a0e7821e11edc
Kaguya <rx700@vip.qq.com>
2025-10-28 18:06:09 +0800

    Revert Fan Curve

--- LenovoLegionToolkit.Lib/Utils/Compatibility.cs
+++ LenovoLegionToolkit.Lib/Utils/Compatibility.cs
@@ -417,7 +417,6 @@ public static partial class Compatibility
             "Legion Pro 5 16IAX10H", // Y7000P With RTX 5070TI
             "LOQ",
             "Y7000", 
-            "R7000"
         };
 
         var (_, type, _, _) = GetModelDataAsync().Result;
