4625a741b463f2bf39f4f482006bd446f0ec3b94
Kaguya <rx700@vip.qq.com>
2026-05-20 10:48:03 +0800

    Added Notification popup for ITSMode to AutomationStep.

--- LenovoLegionToolkit.Lib.Automation/Steps/ITSModeAutomationStep.cs
+++ LenovoLegionToolkit.Lib.Automation/Steps/ITSModeAutomationStep.cs
@@ -1,4 +1,8 @@
-﻿using Newtonsoft.Json;
+﻿using System.Threading;
+using System.Threading.Tasks;
+using LenovoLegionToolkit.Lib.Features;
+using LenovoLegionToolkit.Lib.Listeners;
+using Newtonsoft.Json;
 
 namespace LenovoLegionToolkit.Lib.Automation.Steps;
 
@@ -7,4 +11,16 @@ public class ITSModeAutomationStep(ITSMode state)
     : AbstractFeatureAutomationStep<ITSMode>(state)
 {
     public override IAutomationStep DeepCopy() => new ITSModeAutomationStep(State);
+
+    public override async Task RunAsync(AutomationContext context, AutomationEnvironment environment,
+        CancellationToken token)
+    {
+        var feature = IoCContainer.Resolve<ITSModeFeature>();
+        var currentState = await feature.GetStateAsync().ConfigureAwait(false);
+        if (!State.Equals(currentState))
+            await feature.SetStateAsync(State).ConfigureAwait(false);
+
+        var listener = IoCContainer.Resolve<ITSModeListener>();
+        await listener.OnChangedAsync(State).ConfigureAwait(false);
+    }
 }
