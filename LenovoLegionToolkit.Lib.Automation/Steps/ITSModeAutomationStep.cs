28bd7b5d0b0b4bb051f67627a7c1d57a4f53d35a
Kaguya <rx700@vip.qq.com>
2026-05-20 20:04:12 +0800

    Revert "Added Notification popup for ITSMode to AutomationStep."
    This reverts commit 4625a741b463f2bf39f4f482006bd446f0ec3b94.

--- LenovoLegionToolkit.Lib.Automation/Steps/ITSModeAutomationStep.cs
+++ LenovoLegionToolkit.Lib.Automation/Steps/ITSModeAutomationStep.cs
@@ -1,8 +1,4 @@
-﻿using System.Threading;
-using System.Threading.Tasks;
-using LenovoLegionToolkit.Lib.Features;
-using LenovoLegionToolkit.Lib.Listeners;
-using Newtonsoft.Json;
+﻿using Newtonsoft.Json;
 
 namespace LenovoLegionToolkit.Lib.Automation.Steps;
 
@@ -11,16 +7,4 @@ public class ITSModeAutomationStep(ITSMode state)
     : AbstractFeatureAutomationStep<ITSMode>(state)
 {
     public override IAutomationStep DeepCopy() => new ITSModeAutomationStep(State);
-
-    public override async Task RunAsync(AutomationContext context, AutomationEnvironment environment,
-        CancellationToken token)
-    {
-        var feature = IoCContainer.Resolve<ITSModeFeature>();
-        var currentState = await feature.GetStateAsync().ConfigureAwait(false);
-        if (!State.Equals(currentState))
-            await feature.SetStateAsync(State).ConfigureAwait(false);
-
-        var listener = IoCContainer.Resolve<ITSModeListener>();
-        await listener.OnChangedAsync(State).ConfigureAwait(false);
-    }
 }
